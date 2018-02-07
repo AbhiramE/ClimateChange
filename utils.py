e79e7af16acdf6e7bebb638c7da59002f6eb6e1a
Abhay Mittal <abhaymittal@roc.cs.umass.edu>
2018-02-06 20:08:16 -0500

    Add read_output

---
+++ utils.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import pandas as pd
+
+def read_output(file_name, header_row=0, skip_rows=2, delim=r"\s+"):
+    '''
+    Method to read the output file (fort.22)
+    
+    ----
+    Args:
+    file_name: The path to the output file
+    header_row: The index of the header_row (index considered
+    after the skip_rows have been removed)
+    skip_rows: The number of rows from the beginning to skip. It can
+    also be a tuple of ints denoting which rows to remove specifically
+    delim: The delimiter for the file , default is whitespace
+    '''
+    df=pd.read_csv(file_name,header=header_row,delimiter=delim, skiprows=skip_rows)
+    df=df[:-1] # Remove the last row which contains some summary
+    return
+
