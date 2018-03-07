d7185bf75816c7864023f368b2085b83b8dee0e9
Abhay Mittal <mittalabhay94@gmail.com>
2018-03-07 13:42:34 -0500

    Add class for sampling and GridSampling:

---
+++ sampling/Sampler.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod 
+class Sampler:
+    '''
+    Base class for all the different sampling methods
+    '''
+
+    def __init__(self, param_names, param_ranges):
+        '''
+        Constructor
+        
+        Args:
+        ----
+        param_names:  A list of parameter names
+        param_ranges: A list of parameter valid ranges. Each element of this 
+        list must be a tuple of the form (r_beg, r_end) where r_beg and r_end denote
+        the minimum and maximum values respectively for the parameter range
+        '''
+        self.param_names=param_names
+        self.param_ranges=param_ranges 
+        super().__init__()
+        return
+
+    @abstractmethod
+    def sample(self, num_samples):
+        '''
+        Method which returns a set of samples
+
+        Args:
+        ----
+        num_samples: The number of samples to return 
+        '''
+        pass
+
+
